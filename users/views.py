cd876a5ce1b5dace460d0b35112314f365d3db35
akhink06 <akhinkakhin@gmail.com>
2024-02-05 16:47:44 +0530

    create user error handling

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render , reverse
+
 from django.contrib.auth import authenticate , login as auth_login , logout as auth_logout
 from django.http.response import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+from users.forms import UserForm
+from main.function import generate_form_errors
 
 
 def login(request):
@@ -40,3 +45,42 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect(reverse("EnglishDictionary:index"))
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            isinstance = form.save(commit=False)
+
+            User.objects.create_user(
+                username = isinstance.username,
+                password = isinstance.password,
+                email = isinstance.email,
+                first_name = isinstance.first_name,
+                last_name = isinstance.last_name,
+            )
+
+            user = authenticate(request, username = isinstance.username , password = isinstance.password)
+            auth_login(request,user)
+
+            return HttpResponseRedirect(reverse("EnglishDictionary:index"))
+        else: 
+            message = generate_form_errors(form)
+            
+            form = UserForm()
+            context = {
+                "title" : "Signup",
+                "error" : True,
+                "message" : message,
+                "form" : form ,
+            }
+            return render(request, "users/signup.html",context=context)
+
+    else:
+        form = UserForm()
+        context = {
+            "title" : "signup" ,
+            "form" : form ,
+    }
+    return render(request , "users/signup.html" , context=context)
