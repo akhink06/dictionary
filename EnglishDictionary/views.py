84583b8cb844f77a7aa0a580154e4942510a9d43
akhink06 <akhinkakhin@gmail.com>
2024-02-05 14:48:19 +0530

    login and logout completed

--- EnglishDictionary/views.py
+++ EnglishDictionary/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+context = {
+    "title" : "Home page"
+}
+
+def index(request):
+    return render(request, 'web/index.html', context=context)
